dd7296f4354c6d2d1ee2e09f1311a8ec40e0a462
goEcar <1234519jin>
2017-01-17 15:54:41 +0800

    1.onError 中还出现 error 此时通过订阅者的try catch 打印日志

--- ecarnet/src/main/java/com/ecar/ecarnetwork/base/BaseSubscriber.java
+++ ecarnet/src/main/java/com/ecar/ecarnetwork/base/BaseSubscriber.java
@@ -60,7 +60,7 @@ public abstract class BaseSubscriber<T> extends Subscriber<T> {
     @Override
     public void onError(Throwable e) {
         CommonException ex = null;
-//        try{
+        try{
             if (e instanceof UserException) {   // 用户自定义需要处理的异常
                 /**
                  * 1.自定义异常处理
@@ -123,16 +123,22 @@ public abstract class BaseSubscriber<T> extends Subscriber<T> {
                 //bug写入日志
 //            FileUtil.writeDebugTextFile(errorMsg);//不能简单的这么写，要考虑弄线程池处理
             }
-//        }catch (Exception exception){
+        }catch (Exception exception){
 //            ex = new CommonException(exception, CommonException.FLAG_UNKNOWN);
 ////            onUnifiedError(ex);   //未知错误 这一句需删掉，网络出错会陷入无线循环。
-//            if(ConstantsLib.DEBUG && exception!=null){
-//                exception.printStackTrace();
-//            }
+            resetContext();
+            if(ConstantsLib.DEBUG ){
+                if(e!=null){
+                    e.printStackTrace();
+                }
+                if(exception!=null){
+                    exception.printStackTrace();
+                }
+            }
 ////            else if(ConstantsLib.DEBUG && exception==null){//这一句不应该执行到
 ////                ex.printStackTrace();
 ////            }
-//        }
+        }
     }
 
     @Override
