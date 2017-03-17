718b6702e88bd6c5d19b6998afc2a7e1a10c6c6e
jinzheng519 <jinzheng519@163.com>
2017-03-17 17:38:28 +0800

    解决了debug模式下弹出Toast的问题

--- ecarnet/src/main/java/com/ecar/ecarnetwork/http/util/TagLibUtil.java
+++ ecarnet/src/main/java/com/ecar/ecarnetwork/http/util/TagLibUtil.java
@@ -6,49 +6,51 @@ import android.widget.Toast;
 
 
 public class TagLibUtil {
-	private final static String TAG = "TagLibUtil";
+    private final static String TAG = "TagLibUtil";
 
-	public static void showToast(Context context, String str) {
-		Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
-	}
-	/**
-	 * 显示debug 数据
-	 * 
-	 * @param str
-	 */
-	public static void showLogDebug(String str) {
-		if (ConstantsLib.DEBUG)
-			Log.d(TAG, str);
-	}
+    public static void showToast(Context context, String str) {
+        if (ConstantsLib.DEBUG)
+            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+    }
 
-	/**
-	 * 显示debug 数据
-	 * 
-	 * @param str
-	 */
-	public static void showLogDebug(Class context, String str) {
-		if (ConstantsLib.DEBUG)
-			Log.d(TAG, "<" + context.getName().toString() + ">--" + str);
-	}
+    /**
+     * 显示debug 数据
+     *
+     * @param str
+     */
+    public static void showLogDebug(String str) {
+        if (ConstantsLib.DEBUG)
+            Log.d(TAG, str);
+    }
 
-	public static void showLogError(String str) {
-		if (ConstantsLib.DEBUG)
-			Log.e(TAG, str);
-	}
+    /**
+     * 显示debug 数据
+     *
+     * @param str
+     */
+    public static void showLogDebug(Class context, String str) {
+        if (ConstantsLib.DEBUG)
+            Log.d(TAG, "<" + context.getName().toString() + ">--" + str);
+    }
 
-	/**
-	 * 显示debug 数据
-	 * 
-	 * @param str
-	 */
-	public static void showLogError(Class context, String str) {
-		if (ConstantsLib.DEBUG)
-			Log.e(TAG, "<" + context.getName().toString() + ">--" + str);
-	}
+    public static void showLogError(String str) {
+        if (ConstantsLib.DEBUG)
+            Log.e(TAG, str);
+    }
 
-	public static void showLogDebug(String tag, String content) {
-		if (ConstantsLib.DEBUG)
-			Log.d(tag, content);
-	}
+    /**
+     * 显示debug 数据
+     *
+     * @param str
+     */
+    public static void showLogError(Class context, String str) {
+        if (ConstantsLib.DEBUG)
+            Log.e(TAG, "<" + context.getName().toString() + ">--" + str);
+    }
+
+    public static void showLogDebug(String tag, String content) {
+        if (ConstantsLib.DEBUG)
+            Log.d(tag, content);
+    }
 
 }
