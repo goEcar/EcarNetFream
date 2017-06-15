80c3bd768249af6ae1004485d6d7b806ffd5dae5
goEcar <1234519jin>
2017-06-15 16:21:21 +0800

    1.处理绕过ngis校验

--- ecarnet/src/main/java/com/ecar/ecarnetwork/http/api/ApiBox.java
+++ ecarnet/src/main/java/com/ecar/ecarnetwork/http/api/ApiBox.java
@@ -166,7 +166,7 @@ public class ApiBox {
             return this;
         }
         public Builder veriNgis(boolean veriNgis) {
-            this.veriNgis = veriNgis;//设置 绕过参数
+            this.veriNgis = veriNgis;//设置绕过参数
             return this;
         }
 
