bed9279c94ff269f641eaa68dfd5bd8daec148bd
goEcar <1234519jin>
2018-01-08 15:24:56 +0800

    1.封装获取资产文件证书代码

--- ecarnet/src/main/java/com/ecar/ecarnetwork/util/file/EFileUtil.java
+++ ecarnet/src/main/java/com/ecar/ecarnetwork/util/file/EFileUtil.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,6 +24,73 @@ public class EFileUtil {
 
 
     /**
+     *  从assets目录中复制整个文件夹内容
+     *  @param  context  Context 使用CopyFiles类的Activity
+     *  @param  oldPath  String  原文件路径  如：/aa
+     *  @param  newPath  String  复制后路径  如：xx:/bb/cc
+     */
+    public void copyFilesFassets(Context context,String oldPath,String newPath) {
+        try {
+            String fileNames[] = context.getAssets().list(oldPath);//获取assets目录下的所有文件及目录名
+            if (fileNames.length > 0) {//如果是目录
+                File file = new File(newPath);
+                file.mkdirs();//如果文件夹不存在，则递归
+                for (String fileName : fileNames) {
+                    copyFilesFassets(context,oldPath + "/" + fileName,newPath+"/"+fileName);
+                }
+            } else {//如果是文件
+                InputStream is = context.getAssets().open(oldPath);
+                FileOutputStream fos = new FileOutputStream(new File(newPath));
+                byte[] buffer = new byte[1024];
+                int byteCount=0;
+                while((byteCount=is.read(buffer))!=-1) {//循环从输入流读取 buffer字节
+                    fos.write(buffer, 0, byteCount);//将读取的输入流写入到输出流
+                }
+                fos.flush();//刷新缓冲区
+                is.close();
+                fos.close();
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+    public static InputStream[] getAssetsCers(Context context, String directName) {
+        try {
+            String fileNames[] = context.getAssets().list(directName);//获取assets目录下的所有文件及目录名
+            if (fileNames != null) {
+                int length = fileNames.length;
+                for (int i = 0; i < length; i++) {
+                    fileNames[i] = directName.concat(File.separator).concat(fileNames[i]);
+                }
+                //加载所有证书
+                InputStream[] assetsInputStream = getAssetsInputStream(context, fileNames);
+                return  assetsInputStream;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public static InputStream[] getAssetsInputStream(Context mContext, String... keys) throws
+            IOException {
+        InputStream[] inputStreams = null;
+        if (keys != null && keys.length != 0) {
+            int e = keys.length;
+            inputStreams = new InputStream[e];
+            int i = 0;
+            for (String key : keys) {
+                inputStreams[i] = mContext.getAssets().open(key);
+                i++;
+            }
+        }
+        return inputStreams;
+    }
+    /**
      * 字符串转成流
      */
     public static InputStream[] getInputStreams(String... keys) {
@@ -37,7 +105,7 @@ public class EFileUtil {
         return inputStreams;
     }
 
-    public static InputStream[] getAssetsInputStream(Context mContext, String... keys) {
+    public static InputStream[] getAssetsInputStream1(Context mContext, String... keys) {
         try {
             if (keys == null || keys.length == 0) {
                 return null;
