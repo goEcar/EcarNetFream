d455aba9dc91a2acb6308e8d40b1eb9ed3a995ec
suntaisheng <suntaisheng@qq.com>
2019-10-29 17:56:14 +0800

    没有修改什么，更新一下啊

--- app/src/main/java/com/ecar/ecarnetfream/main/ui/activity/MainActivity.java
+++ app/src/main/java/com/ecar/ecarnetfream/main/ui/activity/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
 
         //FlowableSubscriber  ResourceSubscrible
         Flowable.just("123").subscribe(new Subscriber<String>() {
